6dd7a07b5f6e8d8f9798398ebb3d5be3c5430a8a
unknown <conner.schiissler@gmail.com>
2020-12-03 09:53:43 -0700

    Day 2 part 1 solved

--- 2020/Day2/Day2.ps1
+++ 2020/Day2/Day2.ps1
@@ -1,4 +1,47 @@
-$passwords = Get-Content -Path $PSScriptRoot\input.txt
-foreach ($password in $passwords) {
-    
+<#
+.SYNOPSIS
+--- Day 2: Password Philosophy ---
+Your flight departs in a few days from the coastal airport; the easiest way down to the coast from here is via toboggan.
+
+The shopkeeper at the North Pole Toboggan Rental Shop is having a bad day. "Something's wrong with our computers; we can't log in!" You ask if you can take a look.
+
+Their password database seems to be a little corrupted: some of the passwords wouldn't have been allowed by the Official Toboggan Corporate Policy that was in effect when they were chosen.
+
+To try to debug the problem, they have created a list (your puzzle input) of passwords (according to the corrupted database) and the corporate policy when that password was set.
+
+For example, suppose you have the following list:
+
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+Each line gives the password policy and then the password. The password policy indicates the lowest and highest number of times a given letter must appear for the password to be valid. For example, 1-3 a means that the password must contain a at least 1 time and at most 3 times.
+
+In the above example, 2 passwords are valid. The middle password, cdefg, is not; it contains no instances of b, but needs at least 1. The first and third passwords are valid: they contain one a or nine c, both within the limits of their respective policies.
+
+How many passwords are valid according to their policies?
+
+.AUTHOR
+nullptr
+#>
+
+$passwordPolicies = Get-Content -Path $PSScriptRoot\..\inputs\inputday2.txt
+
+function challenge2version1($fileInput) {
+    foreach ($passwordPolicy in $fileInput) {
+        $parsePasswordFile = $passwordPolicy.Split(" ")
+        $passwordRule = $parsePasswordFile[0].split("-")
+        $requiredLetter = $parsePasswordFile[1][0]
+        $password = $parsePasswordFile[2]
+
+        $occurance = ($password.ToCharArray() | Where-Object { $_ -eq $requiredLetter } | Measure-Object).Count
+        $minRule = [int]$passwordRule[0]
+        $maxRule = [int]$passwordRule[1]
+
+        if ($occurance -ge $minRule -and $occurance -le $maxRule) {
+            $count += 1
+        }
+    }
+    return $count
 }
+
+challenge2version1 -fileInput $passwordPolicies
---
+++ 2020/Day2/Day2.tests.ps1
@@ -0,0 +1,4 @@
+. $PSScriptRoot\Day2.ps1
+
+[string[]]$sampleDataPart1 = "1-13 r: gqdrspndrpsrjfjx", "5-16 j: jjjjkjjzjjjjjfjzjjj"
+
